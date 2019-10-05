180e115bcf28e339046b3dc6ff3ab718c9ae5bd3
anapa96 <ana.p.a@yandex.ru>
2019-10-05 17:39:40 +0400

    1. Написан JS сценарий, который автоматически закрывае модальное окно после его активации 2. Убрана синяя обводка у кнопок

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,14 @@ var close= document.querySelector('#close');
 
 button.addEventListener('click', function(){
   modal.classList.add('modal_active');
+  // первый аргумент - функция
+  function second_passed() {
+    modal.classList.remove('modal_active');
+  }
+  setTimeout(second_passed, 5000);
 });
 
 close.addEventListener('click', function(){
   modal.classList.remove('modal_active');
 });
+
