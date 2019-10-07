a5ea8142701e3ac8c66436b4adbfac50beeb7eac
anapa96 <ana.p.a@yandex.ru>
2019-10-08 00:08:07 +0400

    Исправлен крестик закрытия. Добавлена копка наверх и прописан к ней jQ сценарий

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,4 +8,19 @@ $(document).ready(function(){
   close.on('click', function(){
     modal.removeClass('modal_active');
   });
+
+  const top_show = 150; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+  const delay = 1000; // Задержка прокрутки
+  $(window).scroll(function () { // При прокрутке попадаем в эту функцию
+    /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+    if ($(this).scrollTop() > top_show) $('#top').fadeIn();
+    else $('#top').fadeOut();
+ });
+  $('#top').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
+    /* Плавная прокрутка наверх */
+    $('body, html').animate({
+      scrollTop: 0
+    }, delay);
+  });
+
 });
